adf9b906bad7c21486bfe7e36f7180e471a00f91
Sean Cross <sean@chumby.com>
2011-09-29 22:33:54 +0800

    chumby-netvbrowser: Add keepalive daemon
    Add a daemon to wrap NeTVBrowser and monitor unnatural exits, such
    as when it segfaults.  It shouldn't segfault anymore, but if it
    does, then this daemon will catch that and restart it.

--- recipes/chumby-netvbrowser/chumby-netvbrowser/keepalive.c
+++ recipes/chumby-netvbrowser/chumby-netvbrowser/keepalive.c
@@ -5,8 +5,10 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <syslog.h>
 
-#define PROCESS "/usr/bin/NeTVBrowser", "-qws"
+#define PROCESS "/usr/bin/NeTVBrowser.real"
+//#define PROCESS "./target"
 
 static int child_pid;
 static int should_quit;
@@ -16,37 +18,62 @@ void kill_child(int ignored) {
 	kill(child_pid, SIGTERM);
 }
 
-int main(int argc, char **argv) {
+void child_died(int ignored) {
 	int status;
+	pid_t pid;
+
+	pid = waitpid(child_pid, &status, 0);
+	if (pid == -1) {
+		perror("Couldn't wait for pid");
+		should_quit = 1;
+	}
+	
+	if (WIFEXITED(status))
+		should_quit = 1;
+	else {
+		syslog(LOG_WARNING,
+		     "Child exited %d, signaled %d, returned status: 0x%08x\n",
+		    WEXITSTATUS(status), WTERMSIG(status), status);
+		kill(child_pid, SIGTERM);
+		child_pid = 0;
+	}
+}
+
+int main(int argc, char **argv) {
+	int i;
+	char *my_args[argc+1];
+
+	/* Copy our arguments to the process arguments list */
+	my_args[0] = PROCESS;
+	for(i=1; i<argc; i++)
+		my_args[i] = argv[i];
+	my_args[i] = NULL;
 
 	signal(SIGTERM, kill_child);
 	signal(SIGINT,  kill_child);
-	signal(SIGCHLD, SIG_IGN);
+	signal(SIGCHLD, child_died);
 	signal(SIGSEGV, SIG_IGN);
 	signal(SIGFPE,  SIG_IGN);
 
+	openlog("NeTVBrowser", 0, LOG_LOCAL0);
+
 	while(!should_quit) {
-		pid_t pid;
 
 		/* If there's no child running (or if it died), restart it */
-		fprintf(stderr, "Child PID: %d\n", child_pid);
 		if (child_pid <= 0) {
 			if (!(child_pid = fork())) {
-				execlp(PROCESS, NULL);
-				exit(-1);
+				execvp(PROCESS, my_args);
+				perror("Couldn't start up " PROCESS);
+				exit(0);
 			}
+			syslog(LOG_INFO, "Started child. PID: %d\n", child_pid);
 		}
-				
-		pid = waitpid(child_pid, &status, 0);
-		if (pid == -1)
-			perror("Couldn't wait for pid");
-		
-		if (WIFEXITED(status))
-			should_quit = 1;
-		else {
-			kill(child_pid, SIGTERM);
-			child_pid = 0;
-		}
+
+		/*
+		 * Do nothing, wait for SIGCHLD to hit, which will
+		 * cause select() to return.
+		 */
+		select(0, NULL, NULL, NULL, NULL);
 	}
 
 	return 0;
--- recipes/chumby-netvbrowser/chumby-netvbrowser_1.0.bb
+++ recipes/chumby-netvbrowser/chumby-netvbrowser_1.0.bb
@@ -5,12 +5,14 @@ DESCRIPTION = "Chromeless web browser for NeTV with some added sugar"
 HOMEPAGE = "http://www.chumby.com/"
 AUTHOR = "Torin"
 LICENSE = "GPLv3"
-PR = "r74"
+PR = "r75"
 DEPENDS = "qt4-embedded"
 RDEPENDS_${PN} = "task-qt4e-base"
 
 SRC_URI = "${CHUMBYSG_GIT_HOST}/chumby-sg/${PN}${CHUMBYSG_GIT_EXTENSION};protocol=${CHUMBYSG_GIT_PROTOCOL} \
-           file://setbrowser.c"
+           file://setbrowser.c \
+           file://keepalive.c \
+"
 
 SRCREV = "${AUTOREV}"
 S = "${WORKDIR}/git"
@@ -20,11 +22,13 @@ INITSCRIPT_PARAMS = "defaults 60 40"
 
 do_compile_append() {
     ${CC} ${CFLAGS} ${LDFLAGS} ${WORKDIR}/setbrowser.c -o ${WORKDIR}/setbrowser
+    ${CC} ${CFLAGS} ${LDFLAGS} ${WORKDIR}/keepalive.c -o ${WORKDIR}/keepalive
 }
 
 do_install() {
     install -d ${D}${bindir}
-    install -m 0755 ${S}/bin/NeTVBrowser 			${D}${bindir}
+    install -m 0755 ${S}/bin/NeTVBrowser 			${D}${bindir}/NeTVBrowser.real
+    install -m 0755 ${WORKDIR}/keepalive 			${D}${bindir}/NeTVBrowser
     install -m 0755 ${WORKDIR}/setbrowser			${D}${bindir}
     install -m 0755 ${S}/chumby-netvbrowser-upgrade.sh 		${D}${bindir}
 
